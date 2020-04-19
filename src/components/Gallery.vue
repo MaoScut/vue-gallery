790422a8eb96e55cd4a4552ba80b0d37345e5efe
madp <849345372@qq.com>
2020-04-19 09:53:40 +0800

    feat: 展示图片
    注意, 如果图片路径是运行时才知道的,那就无法打包了.这时候图片只能放在public里面.
    
    路径固定,才有可能在编译时打包并替换img的src属性

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -4,14 +4,29 @@
     <p>list: {{list}}</p>
     <p>isLoading: {{isLoading}}</p>
     <button @click="fetchData">重新获取</button>
+    <GalleryUnit :gallery-picture="pictures[0]"/>
   </div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import GalleryUnit, { GalleryPicture } from './GalleryUnit.vue';
 
-@Component
+@Component({
+  components: {
+    GalleryUnit,
+  },
+})
 export default class Gallery extends Vue {
   @Prop() private msg!: string;
+
+  pictures: GalleryPicture[] = [{
+    title: 'p1',
+    url: '/pictures/wallhaven-15776.jpg',
+    description: 'p1 desc',
+    positionX: 0,
+    positionY: 0,
+    isCenter: false,
+  }];
 
   get list() {
     return this.$store.state.pictures;
---
+++ src/components/GalleryUnit.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{galleryPicture.title}}</p>
+    <img :src="galleryPicture.url" />
+    <p>{{galleryPicture.description}}</p>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import { Picture } from '@/api/picture';
+
+
+export interface GalleryPicture extends Picture {
+  positionX: number;
+  positionY: number;
+  isCenter: boolean;
+}
+
+@Component
+export default class GalleryUnit extends Vue {
+  @Prop() galleryPicture!: GalleryPicture;
+}
+</script>
